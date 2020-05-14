8da5e2f61e187c195b164b863f161146b812a04e
polRk <polrkov.tm@gmail.com>
2020-05-14 16:41:59 +0300

    text: use jsx instead of React pragma

--- test/Button.test.tsx
+++ test/Button.test.tsx
@@ -1,12 +1,13 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+/** @jsx jsx */
+import { jsx } from '@emotion/core'
+import * as ReactDOM from 'react-dom'
 
-import { Button } from '../src';
+import { Button } from '../src'
 
 describe('it', () => {
   it('should renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Button />, div);
-    ReactDOM.unmountComponentAtNode(div);
-  });
-});
+    const div = document.createElement('div')
+    ReactDOM.render(<Button />, div)
+    ReactDOM.unmountComponentAtNode(div)
+  })
+})
